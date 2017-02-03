5411f01dc074261339cd096cc08011745011a93a
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-03 14:51:34 -0600

    classes and methods commented

--- music/music/models.py
+++ music/music/models.py
@@ -1,17 +1,62 @@
 from django.db import models
 
-# Create your models here.
-
-class Artists(models.Model):
+class Genre(models.Model):
     '''
-    The Artists Model contains the essential fields and behaviors of Artists Data.
+    The genre Model contains the essential fields and behaviors of the genre table.
 
     Argument List:
         -models.Model: This argument allows the class to access field types.
 
-    Author: Nate Baker, Python Ponies
+    Author: Nate Baker
+    '''
+    genre_name = models.CharField(max_length=200, blank=True, default='')
+
+    def __str__(self):
+        return '{0}'.format(self.genre_name)
+
+class Artist(models.Model):
+    '''
+    The Artists Model contains the essential fields and behaviors of the Artists table.
+
+    Argument List:
+        -models.Model: This argument allows the class to access field types.
+
+    Author: Nate Baker
     '''
     artist_name = models.CharField(max_length=200, blank=True, default='')
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{0}'.format(self.artist_name)
+
+class Album(models.Model):
+    '''
+    The album Model contains the essential fields and behaviors of the album table.
+
+    Argument List:
+        -models.Model: This argument allows the class to access field types.
+
+    Author: Nate Baker
+    '''
+    album_name = models.CharField(max_length=200, blank=True, default='')
+    artist = models.ForeignKey(Artist, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0}'.format(self.album_name)
+
+class Song(models.Model):
+    '''
+    The Song Model contains the essential fields and behaviors of the Song table.
+
+    Argument List:
+        -models.Model: This argument allows the class to access field types.
+
+    Author: Nate Baker
+    '''
+    song_name = models.CharField(max_length=200, blank=True, default='')
+    song_length = models.IntegerField()
+    artist = models.ForeignKey(Artist, null=True, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{0}'.format(self.song_name)
--- music/music/serializers.py
+++ music/music/serializers.py
@@ -2,20 +2,57 @@ from rest_framework import serializers
 from .models import *
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
-    ''' The ArtistSerializer class translates the Artists models into other formats, in this case JSON by default. that Products table so a database can be created from it.
-
-    Method List:
-    -Meta
-    -create
-    -update
+    ''' This class translates the Artist model into other formats, in this case JSON by default.
 
     Argument List:
     -serializers.HyperlinkedModelSerializer: This argument allows the class to access field types.
 
-    Author: Nate Baker, Python Ponies
+    Author: Nate Baker
+    '''
+    class Meta:
+        ''' This method is tied to the ArtistSerializer class and tells the serializer to serialize all fields in the table.
+        '''
+        model = Artist
+        fields = '__all__'
+
+class SongSerializer(serializers.HyperlinkedModelSerializer):
+    ''' This class translates the Song model into other formats, in this case JSON by default.
+
+    Argument List:
+    -serializers.HyperlinkedModelSerializer: This argument allows the class to access field types.
+
+    Author: Nate Baker
     '''
     class Meta:
         ''' This method is tied to the ProductsSerializer class and tells the serializer to serialize all fields in the table.
         '''
-        model = Artists
+        model = Song
+        fields = '__all__'
+
+class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+    ''' This class translates the Album model into other formats, in this case JSON by default.
+
+    Argument List:
+    -serializers.HyperlinkedModelSerializer: This argument allows the class to access field types.
+
+    Author: Nate Baker
+    '''
+    class Meta:
+        ''' This method is tied to the AlbumSerializer class and tells the serializer to serialize all fields in the table.
+        '''
+        model = Album
+        fields = '__all__'
+
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    ''' This class translates the Genre model into other formats, in this case JSON by default.
+
+    Argument List:
+    -serializers.HyperlinkedModelSerializer: This argument allows the class to access field types.
+
+    Author: Nate Baker
+    '''
+    class Meta:
+        ''' This method is tied to the GenreSerializer class and tells the serializer to serialize all fields in the table.
+        '''
+        model = Genre
         fields = '__all__'
